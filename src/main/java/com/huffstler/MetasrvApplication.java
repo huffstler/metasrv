edce9765fa86340566d561c7b1b403a23869544e
Robert Hitchens <rjhitchens3@gmail.com>
2020-01-29 18:44:36 -0500

    moved application class to root of package for auto scanning

--- src/main/java/com/huffstler/MetasrvApplication.java
+++ src/main/java/com/huffstler/MetasrvApplication.java
@@ -1,12 +1,8 @@
-package com.huffstler.metasrv;
+package com.huffstler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories("com.huffstler.persistence.repo")
-@EntityScan("com.huffstler.persistence.model")
 @SpringBootApplication
 public class MetasrvApplication {
 	public static void main(String[] args) {
